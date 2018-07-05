9260b560e907374ac77d00b66230cd78ad214f11
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-07-05 13:59:03 -0400

    Test Monte Carlo agent on Blackjack problem

---
+++ tests/monte_test.py
@@ -0,0 +1,13 @@
+import gym
+from src.agent import MonteAgent
+
+
+def test_on_blackjack():
+    env = gym.make('Blackjack-v0')
+    agent = MonteAgent(env=env, nb_episodes=100, verbose=True)
+    score = agent.score()
+
+    assert agent.returns_dict != {}
+    assert isinstance(score, float)
+
+
