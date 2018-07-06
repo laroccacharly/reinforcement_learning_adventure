389410a107233a97fd8365f964cf4082b839692f
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-07-06 14:44:38 -0400

    Removed verbose

--- tests/monte_test.py
+++ tests/monte_test.py
@@ -4,7 +4,7 @@ from src.agent import MonteAgent
 
 def test_on_blackjack():
     env = gym.make('Blackjack-v0')
-    agent = MonteAgent(env=env, nb_episodes=100, verbose=True)
+    agent = MonteAgent(env=env, nb_episodes=100)
     score = agent.score()
 
     assert agent.returns_dict != {}
