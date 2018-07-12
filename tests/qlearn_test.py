64841bc762dccbf5c267517ead1ea0d6f6c5f0b2
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-07-12 17:35:22 -0400

    added test for qlearn agent

--- tests/qlearn_test.py
+++ tests/qlearn_test.py
@@ -1,11 +1,23 @@
 import gym
 from src.agent import QlearnAgent
 
+step_size = 0.1
+env = gym.make('FrozenLake-v0')
+agent = QlearnAgent(env=env, nb_episodes=100, step_size=step_size)
 
-def test_on_blackjack():
-    env = gym.make('FrozenLake-v0')
-    agent = QlearnAgent(env=env, nb_episodes=100)
+
+def test_learn():
+    state = 0
+    action = 0
+    reward = 1
+    next_state = 1
+    next_action = 1
+    agent.learn(state, action, reward, next_state, next_action, done=False)
+    assert agent.values[state][action] == step_size * reward
+
+
+def test_score():
+    agent.reset()
     score = agent.score()
-
     assert agent.values != {}
     assert isinstance(score, float)
