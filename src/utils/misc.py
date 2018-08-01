f4fd91eeef05d4f4392906b14a3aab08c5495989
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-08-01 00:38:51 -0400

    added to_var function

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,4 +1,18 @@
 from collections import namedtuple
+import numpy as np
+import torch
+from torch.autograd import Variable
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'reward', 'next_state', 'next_action', 'done'))
+
+def to_var(x):
+    """
+        Because PyTorch works with Variables
+    """
+    x = np.array(x)
+    x = torch.Tensor(x)
+    if torch.cuda.is_available():
+        x.cuda()
+    x = Variable(x)
+    return x
