c2d2c04c952ae738ba8a30c78bb04c5e1b09a67d
Charly Robinson La Rocca <larocca.charly@gmail.com>
2018-08-01 00:29:56 -0400

    Added OUNoise class that generates correlated noise

---
+++ src/utils/noise.py
@@ -0,0 +1,28 @@
+# --------------------------------------
+# Reference: https://github.com/rllab/rllab/blob/master/rllab/exploration_strategies/ou_strategy.py
+# --------------------------------------
+
+import numpy as np
+import numpy.random as nr
+
+class OUNoise:
+    """
+    Generates correlated noise
+    """
+    def __init__(self,action_dimension,mu=0, theta=0.15, sigma=0.3):
+        self.action_dimension = action_dimension
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * nr.randn(len(x))
+        self.state = x + dx
+        return self.state
+
